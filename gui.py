9e835e9431ee45128c42079bb1fa5e4f27bb0a88
Saif-Red <93396760+Saif-Red@users.noreply.github.com>
2026-08-10 01:26:56 +0530

    Create initial Tkinter GUI

---
+++ gui.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+from tkinter import filedialog
+class FileOrganizerGUI:
+    def __init__(self, root):
+        self.root =root
+        self.root.title("Smart File Organizer")
+        self.root.geometry("700x500")
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.title_label = tk.Label(
+            self.root,
+            text = "SMART FILE ORGANIZER",
+            font = ("Arial", 22, "bold")
+        )
+        self.title_label.pack(pady = 30)
+        self.folder_entry = tk.Entry(
+            self.root,
+            width = 55
+        )
+
+        self.folder_entry.pack(pady = 10)
+
+        self.browse_button = tk.Button(
+            self.root,
+            text = "Browse",
+            command = self.browse_folder
+        )
+
+        self.browse_button.pack(pady = 10)
+
+    def browse_folder(self):
+        folder = filedialog.askdirectory()
+        if folder:
+            self.folder_entry.delete(0, tk.END)
+            self.folder_entry.insert(0, folder)
+
+def main():
+    root = tk.Tk()
+
+    app = FileOrganizerGUI(root)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
